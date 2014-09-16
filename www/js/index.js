125a44744db15647d37345fadca77739e94f9174
Nathan-Bisson <nathan_bisson@live.com>
2014-09-16 19:33:42 -0400

    New Commit

--- www/js/index.js
+++ www/js/index.js
@@ -35,6 +35,7 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
         app.loadBooks();
+		navigator.geolocation.getCurrentPosition(success, error, options)
     },
     loadBooks: function() {
         var request = new  XMLHttpRequest();
@@ -64,5 +65,26 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log('Your current position is:');
+  console.log('Latitude : ' + crd.latitude);
+  console.log('Longitude: ' + crd.longitude);
+  console.log('More or less ' + crd.accuracy + ' meters.');
+};
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
 
 app.initialize();
